eb2ec749e9a30428aede7c73a7399085c0c5c870
Arittro7 <arittro007@gmail.com>
2024-11-20 10:55:51 +0600

    Banner

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -1,24 +1,27 @@
+import bnrImg from '../../assets/banner.jpg'
 
 const Banner = () => {
+
   return (
     <div>
       <div
         className="hero min-h-screen"
         style={{
           backgroundImage:
-            "url(https://img.freepik.com/free-photo/workplace-business-modern-male-accessories-laptop-black-background_155003-3944.jpg?t=st=1731552590~exp=1731556190~hmac=7024a02a23eaad43ac339d301d1800b3c06aa361f8aedec4d98570817b8bb28e&w=740)",
+            `url(${bnrImg})`,
         }}
       >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-          <h1 className="text-4xl font-bold px-4 -ml-8">QUICK<span className="text-green-600">Ponno</span></h1>
+          <div className="max-w">
+            <h1 className="text-4xl font-bold mb-4 uppercase">
+              QUICK<span className="text-green-600">Ponno</span>
+            </h1>
             <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              Your One-Stop Shop for All Things You Love. <br /> Shop Smart, Shop
+              Local, Shop PonnoLagbe! <br /> ✨ Enjoy Exclusive Deals, Fast Delivery,
+              and Hassle-Free Shopping ✨
             </p>
-            <button className="btn btn-primary">Get Started</button>
           </div>
         </div>
       </div>
