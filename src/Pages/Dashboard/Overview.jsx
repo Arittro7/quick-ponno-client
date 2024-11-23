e95fbe881855c87fa46ac1340f1c935c981b8e4e
Arittro7 <arittro007@gmail.com>
2024-11-23 23:18:19 +0600

    UI Fixing

--- src/Pages/Dashboard/Overview.jsx
+++ src/Pages/Dashboard/Overview.jsx
@@ -5,6 +5,10 @@ const Overview = () => {
   return (
     <div>
       <h1 className="font-semibold">{user.email}</h1>
+
+      <h1>After deploying, the private routes (sidebar button) specially Wishlist and home are sometimes not working with single click. It require continuous multiple click to work</h1>
+
+      <p>Please do Consider🙏🏼</p>
     </div>
   );
 };
